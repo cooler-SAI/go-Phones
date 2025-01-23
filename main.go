43cce3398a5ef3299d4eda82a377dadcc96ca97f
cooler-SAI <cooler19860212@yahoo.com>
2025-01-23 22:07:32 +0300

    added base code to connect DB

--- main.go
+++ main.go
@@ -1,13 +1,41 @@
 package main
 
 import (
+	"database/sql"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"go-Phones/handlers"
 	"go-Phones/tools"
+	_ "modernc.org/sqlite"
 	"net/http"
 	"os"
 )
+
+const createTableQuery = `
+CREATE TABLE IF NOT EXISTS phones (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    brand TEXT NOT NULL,
+    price REAL NOT NULL
+);`
+
+func connectDB() (*sql.DB, error) {
+	db, err := sql.Open("sqlite", "phones.db")
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
+
+}
+
+func setupDB(db *sql.DB) error {
+	_, err := db.Exec(createTableQuery)
+	return err
+}
 
 func main() {
 
