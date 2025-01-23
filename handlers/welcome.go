7d8d7ec7f3e0d345a7ce7f998d911d12837f1a69
cooler-SAI <cooler19860212@yahoo.com>
2025-01-23 22:30:11 +0300

    set up server, work in progress

--- handlers/welcome.go
+++ handlers/welcome.go
@@ -1,9 +1,43 @@
 package handlers
 
 import (
+	"database/sql"
+	"fmt"
 	"net/http"
 )
 
-func WelcomePage(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "html/welcome.html")
+var DB *sql.DB
+
+func SetDB(db *sql.DB) {
+	DB = db
+}
+
+func WelcomePage(w http.ResponseWriter, _ *http.Request) {
+	rows, err := DB.Query("SELECT name, brand, price FROM phones")
+	if err != nil {
+		http.Error(w, "Failed to fetch data", http.StatusInternalServerError)
+		return
+	}
+	defer func(rows *sql.Rows) {
+		err := rows.Close()
+		if err != nil {
+
+		}
+	}(rows)
+
+	for rows.Next() {
+		var name, brand string
+		var price float64
+		err := rows.Scan(&name, &brand, &price)
+		if err != nil {
+			return
+		}
+		write, err := w.Write([]byte(name + " - " + brand + " - " +
+			fmt.Sprintf("%.2f", price) + "\n"))
+		if err != nil {
+			return
+		}
+		fmt.Printf("Written %d bytes\n", write)
+
+	}
 }
--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"database/sql"
-	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
 	"go-Phones/handlers"
-	"go-Phones/tools"
-	_ "modernc.org/sqlite"
 	"net/http"
 	"os"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	_ "modernc.org/sqlite"
 )
 
 const createTableQuery = `
@@ -18,6 +18,40 @@ CREATE TABLE IF NOT EXISTS phones (
     brand TEXT NOT NULL,
     price REAL NOT NULL
 );`
+
+func main() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+
+	log.Info().Msg("Server starting...")
+
+	db, err := connectDB()
+	if err != nil {
+		log.Fatal().Err(err)
+	}
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+
+		}
+	}(db)
+
+	if err := setupDB(db); err != nil {
+		log.Fatal().Err(err)
+	}
+
+	handlers.SetDB(db)
+
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	http.HandleFunc("/", handlers.WelcomePage)
+
+	log.Info().Msg("Server running at http://localhost:8080")
+	err2 := http.ListenAndServe(":8080", nil)
+	if err2 != nil {
+		return
+	}
+}
 
 func connectDB() (*sql.DB, error) {
 	db, err := sql.Open("sqlite", "phones.db")
@@ -29,28 +63,9 @@ func connectDB() (*sql.DB, error) {
 		return nil, err
 	}
 	return db, nil
-
 }
 
 func setupDB(db *sql.DB) error {
 	_, err := db.Exec(createTableQuery)
 	return err
-}
-
-func main() {
-
-	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
-	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
-
-	log.Info().Msg("Server Starting....")
-
-	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
-
-	http.HandleFunc("/", handlers.WelcomePage)
-
-	log.Info().Msg("Server StopApp Running....")
-	tools.StopApp()
-
-	log.Info().Msg("Main End..")
 }
